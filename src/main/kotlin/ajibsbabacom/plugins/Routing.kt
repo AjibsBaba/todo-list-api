aeefa3ea3fb1e2d4ecc1eb9bb74d1eac6e2059bd
Samuel Ajibade <samuelajibade22@gmail.com>
2023-12-26 13:01:13 +0100

    added endpoints for routing

--- src/main/kotlin/ajibsbabacom/plugins/Routing.kt
+++ src/main/kotlin/ajibsbabacom/plugins/Routing.kt
@@ -1,13 +1,74 @@
 package ajibsbabacom.plugins
 
+import TodoService
+import ajibsbabacom.models.Task
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+
+    val todoService = TodoService()
+
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        route("/") {
+            get {
+                call.respondText("Welcome to Todo List API")
+            }
+        }
+        route("/api/tasks") {
+            get {
+                call.respond(todoService.getAllTasks())
+            }
+            get("/{id}") {
+                val taskId = call.parameters["id"]?.toIntOrNull()
+                if (taskId != null) {
+                    val task = todoService.getTaskbyId(taskId)
+                    if (task != null) {
+                        call.respond(task)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound, "Task not found")
+                    }
+                } else {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid task ID")
+                }
+            }
+            post {
+                val taskRequest = call.receive<Task>()
+                val newTask = todoService.addTask(taskRequest.title, taskRequest.description)
+                call.respond(HttpStatusCode.Created, newTask)
+            }
+            put("/{id}") {
+                val taskId = call.parameters["id"]?.toIntOrNull()
+                if (taskId != null) {
+                    val taskRequest = call.receive<Task>()
+                    val updatedTask = todoService.updateTask(
+                        taskId, taskRequest.title, taskRequest.description, taskRequest.completed
+                    )
+                    if (updatedTask != null) {
+                        call.respond(updatedTask)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound, "Task not found")
+                    }
+                } else {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid task ID")
+                }
+            }
+            delete("/id") {
+                val taskId = call.parameters["id"]?.toIntOrNull()
+                if (taskId != null) {
+                    val deleted = todoService.deleteTask(taskId)
+                    if (deleted == true) {
+                        call.respond(HttpStatusCode.NoContent)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound, "Task not found")
+                    }
+                } else {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid task ID")
+                }
+            }
         }
     }
 }
